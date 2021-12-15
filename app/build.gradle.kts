68fa74927371b749a326e83f56b6d4ba9225fbf4
George <georgukladnik@gmail.com>
2021-12-15 16:26:18 +0300

    Main screen

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     kotlin("android")
     kotlin("kapt")
     kotlin("android.extensions")
+    id("kotlin-android")
 }
 
 android {
@@ -55,6 +56,7 @@ dependencies {
 
     //Room
     implementation("androidx.room:room-ktx:${Version.ROOM}")
+    implementation("androidx.legacy:legacy-support-v4:1.0.0")
 
     /*todo определиться со стеком
     //Moxy - Version.MOXY: 2.1.2
--- app/src/main/java/ru/easygraphics/MainActivity.kt
+++ app/src/main/java/ru/easygraphics/MainActivity.kt
@@ -2,10 +2,12 @@ package ru.easygraphics
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.fragment_charts_list.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        supportFragmentManager.beginTransaction().replace(R.id.container,ChartsListFragment()).commit()
     }
 }
