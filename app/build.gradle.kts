c6a16fdbe7516fca941ab17bb79464b12ea3ee8d
George <georgukladnik@gmail.com>
2021-12-15 15:55:36 +0300

    MPAndroidChart dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,6 +41,8 @@ android {
 }
 
 dependencies {
+    //MPAndroidChart
+    implementation("com.github.PhilJay:MPAndroidChart:v3.1.0")
     //UI
     implementation("com.google.android.material:material:${Version.MATERIAL}")
     implementation("androidx.constraintlayout:constraintlayout:${Version.CONSTRAIN}")
