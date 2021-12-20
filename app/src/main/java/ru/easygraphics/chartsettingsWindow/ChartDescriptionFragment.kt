c0835a6b1427e895bf032c794c65a121611ebd70
George <georgukladnik@gmail.com>
2021-12-20 18:02:15 +0300

    save_and_restore_data_second_fragment

--- app/src/main/java/ru/easygraphics/chartsettingsWindow/ChartDescriptionFragment.kt
+++ app/src/main/java/ru/easygraphics/chartsettingsWindow/ChartDescriptionFragment.kt
@@ -1,5 +1,7 @@
 package ru.easygraphics.chartsettingsWindow
 
+import android.graphics.drawable.ColorDrawable
+import android.graphics.drawable.Drawable
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -35,10 +37,41 @@ class ChartDescriptionFragment : Fragment() {
         return binding.root
     }
 
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        for (i in 0..list.size-1){
+            outState.putString("name_of_the_${i}_column",list[i].first.text.toString())
+            outState.putInt("color_of_the_${i}_chart",(list[i].second.background as ColorDrawable).color)
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding.nameOfThe1Column.setText(savedInstanceState!!.getString("name_of_the_0_column"))
+        binding.colorOfThe1Chart.setBackgroundColor(savedInstanceState!!.getInt("color_of_the_0_chart"))
+        list.add(Pair(binding.nameOfThe1Column,binding.colorOfThe1Chart))
+        var i=1
+        while (savedInstanceState!!.getString("name_of_the_${i}_column")!=null){
+            val llext =binding.namesOfYColumns
+            val llint = LinearLayout(context)
+            llint.layoutParams = LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT,30*requireContext().resources.displayMetrics.density as Int)
+            val et = EditText(context)
+            val v = View(context)
+            et.layoutParams = LinearLayout.LayoutParams(200*requireContext().resources.displayMetrics.density as Int,30*requireContext().resources.displayMetrics.density as Int)
+            v.layoutParams = LinearLayout.LayoutParams(30*requireContext().resources.displayMetrics.density as Int,30*requireContext().resources.displayMetrics.density as Int)
+            llint.addView(et)
+            llint.addView(v)
+            et.setText(savedInstanceState!!.getString("name_of_the_${i}_column"))
+            v.setBackgroundColor(savedInstanceState!!.getInt("color_of_the_0_chart"))
+            llext.addView(llint)
+            list.add(Pair(et,v))
+            i++
+        }
+    }
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding.buttonCancelDescription.setOnClickListener { router.exit() }
-        list.add(Pair(binding.nameOf1Column,binding.colorOfThe1Chart))
+        list.add(Pair(binding.nameOfThe1Column,binding.colorOfThe1Chart))
         binding.buttonAddYColumn.setOnClickListener {
             val llext =binding.namesOfYColumns
             val llint = LinearLayout(context)
@@ -49,6 +82,7 @@ class ChartDescriptionFragment : Fragment() {
             v.layoutParams = LinearLayout.LayoutParams(30*requireContext().resources.displayMetrics.density as Int,30*requireContext().resources.displayMetrics.density as Int)
             llint.addView(et)
             llint.addView(v)
+            v.setBackgroundColor(0)
             llext.addView(llint)
             list.add(Pair(et,v))
         }
