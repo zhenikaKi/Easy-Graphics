853a5059723c5abb3e4e705a697f88e1a94970d3
George <georgukladnik@gmail.com>
2021-12-20 16:06:48 +0300

    some_changes_of_second_fragment

--- app/src/main/java/ru/easygraphics/chartsettingsWindow/ChartDescriptionFragment.kt
+++ app/src/main/java/ru/easygraphics/chartsettingsWindow/ChartDescriptionFragment.kt
@@ -5,13 +5,23 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.github.terrakok.cicerone.Router
+import org.koin.core.qualifier.named
+import org.koin.java.KoinJavaComponent
+import org.koin.java.KoinJavaComponent.getKoin
 import ru.easygraphics.databinding.FragmentChartDescriptionBinding
+import ru.easygraphics.helpers.consts.Scopes
 
 class ChartDescriptionFragment : Fragment() {
     private lateinit var binding: FragmentChartDescriptionBinding
-
+    private val scope = getKoin().createScope<ChartDescriptionFragment>()
+    private val router: Router = scope.get(qualifier = named(Scopes.ROUTER))
     companion object {
-        fun newInstance(): Fragment = ChartDescriptionFragment()
+        fun newInstance(chart_id:Int): Fragment{
+            val cdfragment=ChartDescriptionFragment()
+            cdfragment.arguments=Bundle().apply{putInt("chart_id",chart_id)}
+            return cdfragment
+        }
     }
 
     override fun onCreateView(
@@ -20,5 +30,11 @@ class ChartDescriptionFragment : Fragment() {
     ): View? {
         binding = FragmentChartDescriptionBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.buttonCancelDescription.setOnClickListener { router.exit() }
+
     }
 }
--- app/src/main/java/ru/easygraphics/chartsettingsWindow/ChartDescriptionScreen.kt
+++ app/src/main/java/ru/easygraphics/chartsettingsWindow/ChartDescriptionScreen.kt
@@ -4,6 +4,6 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentFactory
 import com.github.terrakok.cicerone.androidx.FragmentScreen
 
-class ChartDescriptionScreen: FragmentScreen {
-    override fun createFragment(factory: FragmentFactory): Fragment = ChartDescriptionFragment.newInstance()
+class ChartDescriptionScreen(val chart_id:Int): FragmentScreen {
+    override fun createFragment(factory: FragmentFactory): Fragment = ChartDescriptionFragment.newInstance(chart_id)
 }
