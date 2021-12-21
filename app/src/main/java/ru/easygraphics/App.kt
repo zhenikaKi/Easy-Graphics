6767e53eccfa196ac34f844cca5e4cc6f130fcbf
DESKTOP-PV6QE6K\user <jurijs.miscenkovs@gmail.com>
2021-12-21 10:10:44 +0200

    Added DI, that previous commit works

--- app/src/main/java/ru/easygraphics/App.kt
+++ app/src/main/java/ru/easygraphics/App.kt
@@ -2,6 +2,7 @@ package ru.easygraphics
 import android.app.Application
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
+import org.koin.core.module.Module
 import ru.easygraphics.helpers.di.Modules
 
 class App: Application() {
@@ -11,7 +12,10 @@ class App: Application() {
             androidContext(this@App)
             modules(
                 Modules.application,
+                Modules.appActivity,
                 Modules.mainWindow,
+                Modules.chartDescriptionWindow
+                //Modules.tableWindow
             )
         }
     }
--- app/src/main/java/ru/easygraphics/helpers/di/Modules.kt
+++ app/src/main/java/ru/easygraphics/helpers/di/Modules.kt
@@ -1,18 +1,43 @@
 package ru.easygraphics.helpers.di
 
 import androidx.room.Room
+import com.github.terrakok.cicerone.Cicerone
+import com.github.terrakok.cicerone.NavigatorHolder
+import com.github.terrakok.cicerone.Router
+import org.koin.core.qualifier.named
 import org.koin.dsl.module
+import ru.easygraphics.MainActivity
+import ru.easygraphics.chartsettingsWindow.ChartDescriptionFragment
 import ru.easygraphics.data.db.AppDB
 import ru.easygraphics.helpers.consts.DB
+import ru.easygraphics.helpers.consts.Scopes
 import ru.easygraphics.mainWindow.ChartsListFragment
+import ru.easygraphics.tableWindow.TableFragment
 
 object Modules {
     //модуль, содержимое которого должно быть во всем приложении
     val application = module {
-        single<AppDB> {
+        single<AppDB>(qualifier = named(Scopes.DB)) {
             Room.databaseBuilder(get(), AppDB::class.java, DB.NAME)
                 .addCallback(AppDB.InsertDefaultData)
                 .build()
+        }
+
+        //навигация
+        single<Cicerone<Router>>(qualifier = named(Scopes.CICERONE)) {
+            Cicerone.create(Router())
+        }
+        single<NavigatorHolder>(qualifier = named(Scopes.NAVIGATOR)) {
+            get<Cicerone<Router>>(qualifier = named(Scopes.CICERONE)).getNavigatorHolder()
+        }
+        single<Router>(qualifier = named(Scopes.ROUTER)) {
+            get<Cicerone<Router>>(qualifier = named(Scopes.CICERONE)).router
+        }
+    }
+
+    //модуль основной активити
+    val appActivity = module {
+        scope<MainActivity> {
         }
     }
 
@@ -21,4 +46,14 @@ object Modules {
         scope<ChartsListFragment> {
         }
     }
+
+    val chartDescriptionWindow = module {
+        scope<ChartDescriptionFragment> {
+        }
+    }
+
+    /*val tableWindow = module {
+        scope<TableFragment> {
+        }
+    }*/
 }
