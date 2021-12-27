a2195b0315ac6b94e500caeb1e02928acefb3bdb
GeorgeUG02 <75248465+GeorgeUG02@users.noreply.github.com>
2021-12-27 16:58:23 +0300

    Delete app/src/main/java/ru/easygraphics/chartTableWindow directory

--- app/src/main/java/ru/easygraphics/chartTableWindow/TableScreen.kt
+++
@@ -1,9 +0,0 @@
-package ru.easygraphics.chartTableWindow
-
-import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentFactory
-import com.github.terrakok.cicerone.androidx.FragmentScreen
-
-class TableScreen(val chart_id:Long): FragmentScreen {
-    override fun createFragment(factory: FragmentFactory): Fragment = TableFragment.newInstance(chart_id)
-}
