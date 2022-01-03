2f1f8f057b18a330ccfc2b5d1ec90c4a993b0aac
DESKTOP-PV6QE6K\user <jurijs.miscenkovs@gmail.com>
2022-01-03 21:45:24 +0200

    Новы адаптер, новый визуал для таблицы. После удаления элемента, список некоректен

--- app/src/main/java/ru/easygraphics/AppExtensions.kt
+++ app/src/main/java/ru/easygraphics/AppExtensions.kt
@@ -16,11 +16,18 @@ import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
 import ru.easygraphics.data.db.entities.ChartAllData
 import ru.easygraphics.data.db.entities.ChartAllDataViewed
+import ru.easygraphics.data.db.entities.ChartLineData
+import ru.easygraphics.data.domain.LineDetails
 import ru.easygraphics.data.domain.TableLineData
 import ru.easygraphics.helpers.consts.App.LOG_TAG
 
 fun View.click(click: () -> Unit) = setOnClickListener {
     click()
+}
+
+fun View.longClick(click: () -> Unit) = setOnLongClickListener {
+    click()
+    true
 }
 
 fun Fragment.arguments(vararg arguments: Pair<String, Any>): Fragment {
@@ -36,12 +43,17 @@ fun Fragment.toast(string: String?) {
 }
 
 fun TextView.addBottomBorder(width: Int) {
-    val bottomBorder: LayerDrawable = getBorders(Color.GRAY,0, 0, 0, width)
+    val bottomBorder: LayerDrawable = getBorders(Color.GRAY, 0, 0, 0, width)
     background = bottomBorder
 }
 
 fun TextView.addRightBorder(width: Int) {
-    val bottomBorder: LayerDrawable = getBorders(Color.GRAY,0,0, width,0)
+    val bottomBorder: LayerDrawable = getBorders(Color.GRAY, 0, 0, width, 0)
+    background = bottomBorder
+}
+
+fun TextView.addRightBottomBorder(width: Int) {
+    val bottomBorder: LayerDrawable = getBorders(Color.GRAY, 0, 0, width, width)
     background = bottomBorder
 }
 
@@ -83,6 +95,23 @@ fun TextView.addBorders(
 
 fun ChartAllDataViewed.parseToListOfTableLineData(): List<TableLineData> {
 
+    /*val columns: MutableList<Cell> = mutableListOf()
+    columns.add(Cell(id = 0, value = graphicData.chart.xName))
+    columns.addAll(graphicData.lines.map { line -> Cell(id = line.lineId, value = line.name) })
+    val header = RowCell(columns)
+
+    //сформируем основные данные для отображения
+    val data: MutableList<RowCell> = graphicData.values.map { hV ->
+        val columnsData: MutableList<Cell> = mutableListOf()
+        //добавляем колонку со значением по X
+        columnsData.add(Cell(id = hV.horizontalValue.xValueId, value = hV.horizontalValue.value, viewed = CellView.EDIT_STRING))
+        //добавляем колонки со значениями по Y на каждой линии
+        columnsData.addAll(hV.verticalValues.map { vV ->
+            Cell(id = vV?.yValueId, value = vV?.value.toString(), viewed = CellView.EDIT_NUMBER)
+        })
+        RowCell(columnsData)
+    }.toMutableList()*/
+
     val result = mutableListOf<TableLineData>()
 
     val firstRow = mutableListOf<Pair<String, Int>>()
@@ -112,7 +141,7 @@ fun ChartAllDataViewed.parseToListOfTableLineData(): List<TableLineData> {
         firstRow.add(Pair(chartLine.name, maxLength))
     }
 
-    val firstRowData = TableLineData(
+    /*val firstRowData = TableLineData(
         DbId = -1,
         LineId = -1,
         LineName = this.chart.xName,
@@ -134,10 +163,6 @@ fun ChartAllDataViewed.parseToListOfTableLineData(): List<TableLineData> {
 
         newRowData?.let { tableLineData ->
 
-            /*val width: Int = firstRowData.LineValue.first {
-                it.first == horizontValue.horizontalValue.value
-            }.second*/
-
             tableLineData.addLineValue(horizontalValue.horizontalValue.value, firstRowWidth)
 
             horizontalValue.verticalValues.forEach { verticalValue ->
@@ -147,26 +172,34 @@ fun ChartAllDataViewed.parseToListOfTableLineData(): List<TableLineData> {
             Log.d(LOG_TAG, "$newRowData")
             result.add(newRowData)
         }
-    }
+    }*/
 
     return result
+}
+
+private fun columnWidthCalculation(chartLineData: ChartLineData): Int {
+
+    var maxLength = chartLineData.chartLine.name.length
+
+    chartLineData.yValues.forEach { verticalValue ->
+        maxLength =
+            if (maxLength < verticalValue.value.toString().length) verticalValue.value.toString().length else maxLength
+    }
+
+    maxLength = if (maxLength < ExtConst.MinColumnWidth) ExtConst.MinColumnWidth else maxLength
+
+    return maxLength
 }
 
 fun ChartAllData.parseToListOfTableLineData(): List<TableLineData> {
 
     val result = mutableListOf<TableLineData>()
 
-    val firstRow = mutableListOf<Pair<String, Int>>()
-    firstRow.add(Pair(this.chart.xName, this.chart.xName.length))
+    val firstRow = mutableListOf<LineDetails>()
+    firstRow.add(LineDetails(this.chart.xName, this.chart.xName.length, true))
 
     this.lines.forEach {
-        var maxLength = it.chartLine.name.length
-
-        it.yValues.forEach { verticalValue ->
-            maxLength =
-                if (maxLength < verticalValue.value.toString().length) verticalValue.value.toString().length else maxLength
-        }
-        firstRow.add(Pair(it.chartLine.name, maxLength))
+        firstRow.add(LineDetails(it.chartLine.name, columnWidthCalculation(it), true))
     }
 
     val firstRowData = TableLineData(
@@ -184,13 +217,13 @@ fun ChartAllData.parseToListOfTableLineData(): List<TableLineData> {
             DbId = this.xValues[i].xValueId,
             LineId = lineId,
             LineName = this.xValues[i].value,
-            LineValue = mutableListOf<Pair<String, Int>>()
+            LineValue = mutableListOf<LineDetails>()
         )
-        newRowData.addLineValue(newRowData.LineName, firstRowData.LineValue[0].second)
+        newRowData.addLineValue(newRowData.LineName, firstRowData.LineValue[0].Width, false)
 
         for (l in this.lines.indices) {
             val yValue = this.lines[l].yValues[i].value.toString()
-            newRowData.addLineValue(yValue, firstRowData.LineValue[l + 1].second)
+            newRowData.addLineValue(yValue, firstRowData.LineValue[l + 1].Width, false)
         }
 
         Log.d(LOG_TAG, "$newRowData")
@@ -200,4 +233,8 @@ fun ChartAllData.parseToListOfTableLineData(): List<TableLineData> {
     }
 
     return result
+}
+
+object ExtConst {
+    const val MinColumnWidth = 5
 }
--- app/src/main/java/ru/easygraphics/tableWindow/adapter/TableAdapter.kt
+++ app/src/main/java/ru/easygraphics/tableWindow/adapter/TableAdapter.kt
@@ -1,11 +1,14 @@
 package ru.easygraphics.tableWindow.adapter
 
 import android.content.Context
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.ListAdapter
 import ru.easygraphics.R
+import ru.easygraphics.data.domain.LineDetails
 import ru.easygraphics.data.domain.TableLineData
+import ru.easygraphics.helpers.consts.App
 
 class TableAdapter(
     private val delegate: Delegate?
@@ -13,9 +16,39 @@ class TableAdapter(
 
     interface Delegate {
         fun onRowSelected(tableLineData: TableLineData)
+        fun onDeleteSelected(tableLineData: TableLineData)
     }
 
     private lateinit var context: Context
+    private val removedItems = arrayListOf<TableLineData>()
+
+    private fun removeItem(position: Int): TableLineData? {
+        if (position >= itemCount) return null
+        val item = currentList[position]
+        Log.d(App.LOG_TAG, "$item")
+        removedItems.add(item)
+        val actualList = currentList - removedItems
+        Log.d(App.LOG_TAG, "Actual size: " + actualList.size.toString())
+        if (actualList.isEmpty()) removedItems.clear()
+        submit(actualList, true)
+        return item
+    }
+
+    fun removeItem(item: TableLineData): TableLineData? {
+        val position = currentList.indexOf(item)
+        Log.d(App.LOG_TAG, "$position")
+        if (position == -1) return null
+        return removeItem(position)
+    }
+
+    private fun submit(list: List<TableLineData>?, isLocalSubmit: Boolean) {
+        if (!isLocalSubmit) removedItems.clear()
+        super.submitList(list)
+    }
+
+    override fun submitList(list: List<TableLineData>?) {
+        submit(list, false)
+    }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TableViewHolder {
         context = parent.context
@@ -26,8 +59,9 @@ class TableAdapter(
         return TableViewHolder(rootView)
     }
 
-    override fun onBindViewHolder(holder: TableViewHolder, position: Int) =
-        holder.bind(getItem(position), context = context, delegate)
+    override fun onBindViewHolder(holder: TableViewHolder, position: Int) {
+        //holder.bind(getItem(position), context = context, delegate)
+}
 
     override fun getItemId(position: Int) = position.toLong()
 
