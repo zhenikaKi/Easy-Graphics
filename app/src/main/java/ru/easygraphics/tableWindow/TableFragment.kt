5417ebbf6cfb25ee31281e4d1a56aa4515038101
DESKTOP-PV6QE6K\user <jurijs.miscenkovs@gmail.com>
2022-01-05 00:41:47 +0200

    Adapter - works, AdapterV - doesn't, remove item doesn't work as expexted

--- app/src/main/java/ru/easygraphics/tableWindow/TableFragment.kt
+++ app/src/main/java/ru/easygraphics/tableWindow/TableFragment.kt
@@ -1,25 +1,27 @@
 package ru.easygraphics.tableWindow
 
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.lifecycleScope
-import kotlinx.coroutines.flow.collect
 import org.koin.core.qualifier.named
 import org.koin.java.KoinJavaComponent.getKoin
 import ru.easygraphics.arguments
 import ru.easygraphics.baseobjects.BaseFragment
-import ru.easygraphics.data.domain.LineDetails
+import ru.easygraphics.data.db.entities.ChartAllData
 import ru.easygraphics.data.domain.TableLineData
 import ru.easygraphics.databinding.FragmentTableBinding
+import ru.easygraphics.helpers.consts.App
 import ru.easygraphics.helpers.consts.Scopes
+import ru.easygraphics.parseToListOfTableLineData
+import ru.easygraphics.states.BaseState
+import ru.easygraphics.states.TableState
 import ru.easygraphics.tableWindow.adapter.TableAdapter
 import ru.easygraphics.tableWindow.adapter.TableAdapterV
 import ru.easygraphics.toast
-import java.text.FieldPosition
 
 class TableFragment : BaseFragment<FragmentTableBinding>(FragmentTableBinding::inflate),
-    TableAdapterV.Delegate {
+    TableAdapter.Delegate {
 
     companion object {
         private const val ARG_CHART_ID = "argument_chart_id"
@@ -42,28 +44,42 @@ class TableFragment : BaseFragment<FragmentTableBinding>(FragmentTableBinding::i
     }
 
     private val scope = getKoin().createScope<TableFragment>()
+
     private val tableViewModel: TableViewModel =
         scope.get(qualifier = named(Scopes.TABLE_VIEW_MODEL))
 
     //private val router: Router = scope.get(qualifier = named(Scopes.ROUTER))
 
-    private val adapter: TableAdapterV = TableAdapterV(delegate = this)
+    //private val tableAdapterV: TableAdapterV = TableAdapterV(delegate = this)
+    private val tableAdapter: TableAdapter = TableAdapter(delegate = this)
 
-    override fun onCreate(savedInstanceState: Bundle?) {
+    private var tableLineList = ArrayList<TableLineData>()
+
+
+    /*override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         if (savedInstanceState == null) {
             chartId?.let { tableViewModel.fetchTableRows(chartId = it) }
         }
-    }
+    }*/
+
+    /*override fun initAfterCreate() {
+        //super.initAfterCreate()
+        chartId?.let { model.fetchTableRows(chartId = it) }
+    }*/
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.graphName.text = chartName
-        binding.table.adapter = adapter
+        binding.table.adapter = tableAdapter
 
-        viewLifecycleOwner.lifecycleScope.launchWhenStarted {
+        binding.graphName.text = "tfht"
+        tableViewModel.getLiveData().observe(viewLifecycleOwner, { renderData(it) })
+
+        chartId?.let { tableViewModel.fetchTableRows(it) }
+
+        /*viewLifecycleOwner.lifecycleScope.launchWhenStarted {
             tableViewModel.error.collect {
                 this@TableFragment.toast(it)
             }
@@ -73,7 +89,24 @@ class TableFragment : BaseFragment<FragmentTableBinding>(FragmentTableBinding::i
             tableViewModel.rowData.collect { rowData ->
                 adapter.setData(rowData)
             }
+        }*/
+    }
+
+    private fun renderData(state: BaseState) {
+        when (state) {
+            is BaseState.Loading -> {
+            }
+            is TableState.Success -> showTable(state.data)
+
+            is BaseState.ErrorState -> Log.d(App.LOG_TAG, state.text)
         }
+    }
+
+    private fun showTable(data: ChartAllData) {
+        //tableAdapter.setData(data.parseToListOfTableLineData())
+        //tableAdapter.submitList(data.parseToListOfTableLineData())
+        tableLineList = data.parseToListOfTableLineData() as ArrayList<TableLineData>
+        tableAdapter.setData(tableLineList)
     }
 
     override fun onRowSelected(tableLineData: TableLineData) {
@@ -82,6 +115,10 @@ class TableFragment : BaseFragment<FragmentTableBinding>(FragmentTableBinding::i
 
     override fun onDeleteSelected(position: Int) {
         this.toast("$position to delete")
-        adapter.removeItem(position)
+        val item = tableLineList[position]
+        val list = ArrayList<TableLineData>(tableLineList)
+        list.remove(item)
+        tableLineList = list
+        tableAdapter.setData(list)
     }
 }
--- app/src/main/java/ru/easygraphics/tableWindow/adapter/TableAdapter.kt
+++ app/src/main/java/ru/easygraphics/tableWindow/adapter/TableAdapter.kt
@@ -1,54 +1,59 @@
 package ru.easygraphics.tableWindow.adapter
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.recyclerview.widget.AsyncListDiffer
 import androidx.recyclerview.widget.ListAdapter
 import ru.easygraphics.R
-import ru.easygraphics.data.domain.LineDetails
 import ru.easygraphics.data.domain.TableLineData
 import ru.easygraphics.helpers.consts.App
 
 class TableAdapter(
-    private val delegate: Delegate?
+    private val delegate: Delegate
 ) : ListAdapter<TableLineData, TableViewHolder>(TableDiffUtil) {
 
     interface Delegate {
         fun onRowSelected(tableLineData: TableLineData)
-        fun onDeleteSelected(tableLineData: TableLineData)
+        fun onDeleteSelected(position: Int)
     }
 
     private lateinit var context: Context
     private val removedItems = arrayListOf<TableLineData>()
 
-    private fun removeItem(position: Int): TableLineData? {
-        if (position >= itemCount) return null
-        val item = currentList[position]
-        Log.d(App.LOG_TAG, "$item")
-        removedItems.add(item)
-        val actualList = currentList - removedItems
-        Log.d(App.LOG_TAG, "Actual size: " + actualList.size.toString())
-        if (actualList.isEmpty()) removedItems.clear()
-        submit(actualList, true)
-        return item
+    private val differ = AsyncListDiffer(this, TableDiffUtil)
+
+    fun setData(tableData: List<TableLineData>){
+        differ.submitList(tableData)
     }
 
-    fun removeItem(item: TableLineData): TableLineData? {
-        val position = currentList.indexOf(item)
+    fun deleteElement(position: Int) {
+        val item = differ.currentList[position]
+        val list = differ.currentList
+        list.remove(item)
+        differ.submitList(list)
+
+        /*currentList.removeAt(position)
+        submit(currentList, true)
+        notifyDataSetChanged()*/
+    }
+
+    /*fun deleteElement(position: Int) {
         Log.d(App.LOG_TAG, "$position")
-        if (position == -1) return null
+        //if (position == -1) return null
         return removeItem(position)
-    }
+    }*/
 
-    private fun submit(list: List<TableLineData>?, isLocalSubmit: Boolean) {
+    /*private fun submit(list: List<TableLineData>?, isLocalSubmit: Boolean) {
         if (!isLocalSubmit) removedItems.clear()
         super.submitList(list)
     }
 
     override fun submitList(list: List<TableLineData>?) {
         submit(list, false)
-    }
+    }*/
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TableViewHolder {
         context = parent.context
@@ -60,10 +65,12 @@ class TableAdapter(
     }
 
     override fun onBindViewHolder(holder: TableViewHolder, position: Int) {
-        //holder.bind(getItem(position), context = context, delegate)
+        holder.bind(differ.currentList[position], context = context, delegate)
 }
 
     override fun getItemId(position: Int) = position.toLong()
 
     override fun getItemViewType(position: Int) = position
+
+    override fun getItemCount(): Int = differ.currentList.size
 }
