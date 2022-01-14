6839c465701cc0dd026d6ca03a5aff1fd14cca1a
George <georgukladnik@gmail.com>
2022-01-14 13:22:02 +0300

    fix

--- app/src/main/java/ru/easygraphics/mainWindow/ChartsListFragment.kt
+++ app/src/main/java/ru/easygraphics/mainWindow/ChartsListFragment.kt
@@ -73,13 +73,17 @@ class ChartsListFragment :
                 router.navigateTo(ChartDescriptionScreen(chartIdItem!!))
             }
             R.id.delete -> {
-                AlertDialog.Builder(requireContext()).setTitle(getString(R.string.dialog_title)).setMessage(getString(R.string.dialog_message)).setCancelable(false).setNegativeButton(
-                    getString(R.string.dialog_negative_button),{ dialog,_ -> dialog.cancel()}).setPositiveButton(
-                    getString(R.string.dialog_positive_button),
-                 { dialogInterface: DialogInterface, i: Int ->
+                AlertDialog.Builder(requireContext()).
+                setTitle(getString(R.string.dialog_title)).
+                setMessage(getString(R.string.dialog_message)).
+                setCancelable(false).
+                setNegativeButton(getString(R.string.dialog_negative_button),{ dialog,_ -> dialog.cancel()}).
+                setPositiveButton(getString(R.string.dialog_positive_button), { dialogInterface: DialogInterface, i: Int ->
                         model.deleteChart(chartIdItem!!)
                         adapter.removeItem(index!!)
-                }).create().show()
+                })
+                .create()
+                .show()
 
             }
         }
